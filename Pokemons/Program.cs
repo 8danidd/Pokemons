149aa2d41fa3708226952e88a93467321934bc01
Daniel . <sngelaneagu5620@gmail.com>
2024-10-27 07:08:35 +0100

    Explorar para encontrar pokemons

--- Pokemons/Program.cs
+++ Pokemons/Program.cs
@@ -7,6 +7,7 @@ public class Lospokemons
     static string[] pokemon = { "Bulbasaur", "Pikachu", "Charmander", "Onix", "Squirtle", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Gengar", "Machop", "Geodude", "Eevee", "Vulpix", "Magikarp" };
     static string[] ashPok = new string[5];
     static string[] garyPok = new string[5];
+    static string[] found = new string[1];
     static string player = "none";
 
     public static void Main(string[] args)
@@ -49,7 +50,7 @@ public class Lospokemons
     static void ShowTeam()
     {
         Console.Clear();
-        Console.WriteLine("Pulsa la tecla para elegir al jugador:\n Ash (Pulsa A)\n Gary (Pulsa S)\n");
+        Console.WriteLine("Pulsa la tecla para elegir al jugador:\nAsh (Pulsa A)\nGary (Pulsa S)\n");
         ConsoleKeyInfo KeyInfo = Console.ReadKey(true);
         if (KeyInfo.Key == ConsoleKey.A) { player = "Ash"; }
         if (KeyInfo.Key == ConsoleKey.S) { player = "Gary"; }
@@ -76,7 +77,12 @@ public class Lospokemons
         if (KeyInfo2.Key == ConsoleKey.Y) { DeletePok(); }
         else
         {
-            ShowTeam();
+            Console.WriteLine("\nQuieres buscar mas pokemons?\n");
+            Console.Write("Si: Y\nNo: N\n");
+            ConsoleKeyInfo KeyInfo3 = Console.ReadKey(true);
+            if (KeyInfo3.Key == ConsoleKey.Y) {
+                Explorar();
+            } else { ShowTeam(); }
         }
     }
 
@@ -107,6 +113,83 @@ public class Lospokemons
         {
             pokemons[pos] = "(VACIO)";
             ShowTeam();
-        } else { Console.Clear(); Console.WriteLine("Posicion no valida"); Thread.Sleep(750); DeletePok(); }
+        } else { 
+            Console.Clear(); 
+            Console.WriteLine("Posicion no valida"); 
+            Thread.Sleep(750); 
+            DeletePok(); 
+        }
+    }
+
+    static void AddPokemon(string newPok)
+    {
+        if (player == "Ash")
+        {
+            for (int i = 0; i < ashPok.Length; ++i) { Console.Write(ashPok[i] + " " + i + ", "); }
+            Console.Write("\n\nIntroduce la posicion:");
+            int posPok = Convert.ToInt32(Console.ReadLine());
+            NewPok(ashPok, posPok);
+        }
+        else
+        {
+            for (int i = 0; i < garyPok.Length; ++i) { Console.Write(garyPok[i] + " " + i + ", "); }
+            Console.Write("\n\nIntroduce la posicion:");
+            int posPok = Convert.ToInt32(Console.ReadLine());
+            NewPok(garyPok, posPok);
+        }
+    }
+
+    static void NewPok(string[] pokemons, int posPok)
+    {
+        if (posPok >= 0 && posPok < pokemons.Length)
+        {
+            pokemons[posPok] = found[0];
+            ShowTeam();
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Posicion no valida");
+            Thread.Sleep(750);
+            ShowTeam();
+        }
+    }
+
+    static void PantallaCarga()
+    {
+        Console.Write("Buscando Pokemons... [");
+        Thread.Sleep(500);
+        for (int i = 0; i < 30; ++i)
+        {
+            Console.Write("#");
+            Thread.Sleep(125);
+        }
+        Console.Write("]");
+        Thread.Sleep(1500);
+    }
+
+    static void Explorar()
+    {
+        Console.Clear();
+        PantallaCarga();
+        Random random = new Random();
+        Random random2 = new Random();
+        int probabilidad = random.Next(0,100);
+        if (probabilidad > 50)
+        {
+            Console.Clear();
+            Console.WriteLine("¡¡¡HAS CAPTURADO UN POKEMON!!!\n");
+            int randomIndex2 = random2.Next(pokemon.Length);
+            found[0] = pokemon[randomIndex2];
+            Console.WriteLine(Convert.ToString(found[0] + "\n"));
+            AddPokemon(found[0]);
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("No has encontrado nada");
+            Thread.Sleep(2000);
+            ShowTeam();
+        }
     }
 }
